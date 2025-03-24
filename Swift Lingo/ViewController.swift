6429c736fc4810faa03e5e56095af8fbd45660c8
Magnus Freidenfelt <sabbathmagnus@gmail.com>
2025-03-24 14:25:44 +0100

    Initial Commit

---
+++ Swift Lingo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Lingo
+//
+//  Created by Magnus Freidenfelt on 2025-03-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
